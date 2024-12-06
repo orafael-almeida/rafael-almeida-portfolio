42eaa12297d1630d8f223f4170a2701c0d0accd8
Rafael Almeida <almeidarp.dev@gmail.com>
2024-12-06 18:08:36 -0300

    feat: creates stars-element component  and hero section styles

---
+++ src/components/StarsElement.tsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useRef } from "react";
+
+const StarsBackground: React.FC = () => {
+  const starsRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const createRandomStar = () => {
+      const star = document.createElement("div");
+      const x = Math.random() * 100;
+      const y = Math.random() * 100;
+      const size = Math.random() * 2 + 1;
+
+      const animationDelay = Math.random() * 5 + "s";
+
+      star.classList.add(
+        "star",
+        "absolute",
+        "bg-white",
+        "rounded-full",
+        "opacity-80"
+      );
+      star.setAttribute(
+        "style",
+        `left: ${x}vw; top: ${y}vh; width: ${size}px; height: ${size}px; animation: twinkle 5s infinite ${animationDelay};`
+      );
+
+      star.addEventListener("animationend", () => {
+        createRandomStar();
+        star.remove();
+      });
+
+      starsRef.current?.appendChild(star);
+    };
+
+    for (let i = 0; i < 30; i++) {
+      createRandomStar();
+    }
+
+    const handleMouseMove = (event: MouseEvent) => {
+      const { clientX, clientY } = event;
+      const rect = starsRef.current?.getBoundingClientRect();
+      if (rect) {
+        const x = ((clientX - rect.left) / rect.width - 0.5) * 2;
+        const y = ((clientY - rect.top) / rect.height - 0.5) * 2;
+        starsRef.current!.style.transform = `translate(${x * 10}px, ${
+          y * 10
+        }px)`;
+      }
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  return (
+    <div
+      ref={starsRef}
+      className="fixed top-0 left-0 w-full h-full z-[-1] pointer-events-none"
+    ></div>
+  );
+};
+
+export default StarsBackground;
--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -1,9 +1,71 @@
+"use client";
+
+import ImgEffect from "@/assets/grain.jpg";
+import StarsBackground from "@/components/StarsElement";
+import { FiGithub, FiInstagram, FiLinkedin } from "react-icons/fi";
+
+const MOCK_CONTACTS = [
+  {
+    url: "https://github.com/orafael-almeida",
+    icon: <FiGithub />,
+  },
+  {
+    url: "https://linkedin.com/in/orafael-almeida",
+    icon: <FiLinkedin />,
+  },
+  {
+    url: "https://instagram.com/orafa-almeida",
+    icon: <FiInstagram />,
+  },
+];
+
 const HeroSection = () => {
   return (
-    <div>
-      <div className="container"></div>
+    <div className="py-32 md:py-48 lg:py-56 relative z-0 h-dvh flex items-center before:absolute before:inset-0 before:bg-[radial-gradient(circle,_rgba(0,0,0,0)_620px,_rgba(0,0,0,0.8)_100%)]">
+      <div
+        className="absolute inset-0 -z-30 opacity-5"
+        style={{ backgroundImage: `url(${ImgEffect.src})` }}
+      ></div>
+      <StarsBackground />
+      <div className="size-[500px]  md:size-[700px] lg:size-[820px] hero-ring relatve"></div>
+      <div className="size-[700px] md:size-[1120px] lg:size-[1220px] hero-ring" />
+      <div className="size-[900px] md:size-[1520px] lg:size-[1620px] hero-ring" />
+      <div className="size-[1100px] md:size-[1720px] lg:size-[1920px] hero-ring" />
+      <div className="container">
+        <div className="flex flex-col items-center">
+          <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
+            <div className="bg-green-500 size-2.5 rounded-full"></div>
+            <div className="text-sm font-medium">
+              Disponível para novos desafios
+            </div>
+          </div>
+        </div>
+        <div className="max-w-lg mx-auto">
+          <h1 className=" text-3xl md:text-5xl text-center mt-8 tracking-wide">
+            RAFAEL ALMEIDA
+          </h1>
+          <p className="mt-4 text-center text-white/60 md:text-lg">
+            Especialista em transfomar ideias e design em soluções funcionais de
+            alta performance.
+          </p>
+        </div>
+        <div className="flex flex-col md:flex-row justify-center items-center mt-8">
+          <div className="text-3xl text-gray-600 flex items-center h-20 gap-3">
+            {MOCK_CONTACTS.map((contact, index) => (
+              <a
+                href={contact.url}
+                key={`contact-${index}`}
+                target="_blank"
+                className="hover:text-gray-100 transform transition-all duration-300 hover:-translate-y-1"
+              >
+                {contact.icon}
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
