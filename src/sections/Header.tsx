e104760c187385badc220a4edeedc0162e5219ba
Rafael Almeida <almeidarp.dev@gmail.com>
2024-12-06 18:07:45 -0300

    feat: adds logo in navbar and applies z-index

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,15 +1,20 @@
+"use client";
+
 import ThemeToggle from "@/components/ThemeToggle";
+import Logo from "@/assets/logo.svg";
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center fixed top-3 w-full px-4 md:px-16 lg:px-32">
-      <div>LOGO</div>
+    <header className="flex justify-between items-center fixed top-3 w-full px-4 md:px-16 lg:px-32 z-50">
+      <div>
+      <Logo className="size-8" />
+      </div>
       <nav className="flex gap-1 p-0.5 border rounded-full border-gray-900/60 bg-gray-200/40 dark:border-white/15 dark:bg-white/10 backdrop-blur">
         <a href="#" className="nav-item text-gray-900 dark:text-slate-200">
           Home
         </a>
         <a href="#" className="nav-item text-gray-900 dark:text-slate-200">
-          Projectos
+          Projetos
         </a>
         <a href="#" className="nav-item text-gray-900 dark:text-slate-200">
           Sobre
