7fe22688db57b0d8145552781087239991178f6b
Rafael Almeida <almeidarp.dev@gmail.com>
2024-12-09 18:39:04 -0300

    feat: creates section title component

---
+++ src/components/SectionTitle.tsx
@@ -0,0 +1,27 @@
+const SectionTitle = ({
+  title,
+  eyebrow,
+  description,
+}: {
+  title: string;
+  eyebrow?: string;
+  description: string;
+}) => {
+  return (
+    <>
+      <div className="flex justify-center">
+        <p className=" font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400  text-center bg-clip-text text-transparent">
+            ../{eyebrow}
+            </p>
+      </div>
+      <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">
+        {title}
+      </h2>
+      <p className="text-center md:text-lg text-white/60 mt-4 mx-auto">
+        {description}
+      </p>
+    </>
+  );
+};
+
+export default SectionTitle;
