462ac055c756125433440efff9108a58e7ac7f48
Rafael Almeida <almeidarp.dev@gmail.com>
2024-12-18 17:31:23 -0300

    fix: adjust general button styles

--- src/components/ButtonGeneral.tsx
+++ src/components/ButtonGeneral.tsx
@@ -6,18 +6,18 @@ const ButtonGeneral = ({
   Icon,
 }: {
   title: string;
-  link: string;
-  Icon: IconType;
+  link?: string;
+  Icon?: IconType;
 }) => {
   return (
     <a
       href={link}
       target="_blank"
       rel="noopener noreferrer"
-      className="cursor-pointer relative inline-flex items-center justify-center p-0.5 text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-emerald-300 to-sky-400 group-hover:from-emerald-300 group-hover:to-sky-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+      className="cursor-pointer relative inline-flex items-center justify-center p-0.5 text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-emerald-300 to-sky-400 group-hover:from-emerald-300 group-hover:to-sky-400 hover:text-white dark:text-white"
     >
       <span className="relative px-2 py-1.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 flex gap-2">
-        <Icon className="self-center size-5" />
+        {Icon && <Icon className="self-center size-5" />}
         {title}
       </span>
     </a>
